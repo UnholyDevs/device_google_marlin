925b9ef65b4e612551f2de5d039e3ee54cfdc298
Bamavic <bamavic1971@gmail.com>
2017-01-02 20:38:09 -0500

    call unholy_defconfig

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -114,7 +114,7 @@ BOARD_EGL_CFG := device/google/marlin/egl.cfg
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/google/marlin
-TARGET_KERNEL_CONFIG := pure_defconfig
+TARGET_KERNEL_CONFIG := unholy_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 BOARD_KERNEL_BASE        := 0x80000000
