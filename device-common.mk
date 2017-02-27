4e213d013971bd670ecc3ae04d4849ad3c756548
DespairFactor <matthewalex@linux.com>
2017-02-27 01:14:14 -0500

    Revert "common: "make" now works!!"
    This reverts commit e2ffb6d85b889d503db3e4ff0fe85acc4b03d1e6.

--- device-common.mk
+++ device-common.mk
@@ -69,19 +69,6 @@ PRODUCT_PACKAGES += fs_config_files \
 # Update engine
 PRODUCT_PACKAGES += brillo_update_payload
 
-# For "make bacon" to work properly
-PRODUCT_PACKAGES += mksquashfsimage.sh \
-                    imgdiff \
-                    bsdiff \
-                    shflags \
-                    aapt \
-                    mkf2fuserimg.sh \
-                    img2simg \
-                    mksquashfs \
-                    delta_generator \
-                    brillo_update_payload \
-                    libprotobuf-cpp-full
-
 # Audio configuration
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
