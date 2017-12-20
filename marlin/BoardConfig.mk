aecc16815db289bab7525f90752faa137de41917
mydongistiny <jaysonedson@gmail.com>
2017-12-20 14:48:17 -0800

    marlin: Don't use system_other for odex
    Change-Id: I33b851975f34cf1b9345dcc796e17e0a1bda0a00
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -202,10 +202,9 @@ TARGET_USE_MDTP := true
 
 TARGET_BOARD_KERNEL_HEADERS := device/google/marlin/kernel-headers
 
-# Install odex files into the other system image
-BOARD_USES_SYSTEM_OTHER_ODEX := true
-
 -include vendor/google_devices/marlin/BoardConfigVendor.mk
+
+-include vendor/google/marlin/BoardConfigVendor.mk
 # Build a separate vendor.img
 TARGET_COPY_OUT_VENDOR := vendor
 
